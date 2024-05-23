68a9dddd373f24ec989f12324fe3f6bdd905a2e2
Leonid Kulik <lyonyauei@gmail.com>
2024-05-23 12:44:45 +0200

    Monday: largest of four

--- intermediate/monday.js
+++ intermediate/monday.js
@@ -1,3 +1,23 @@
 const largestOfFour = arr => arr.map(subArray => Math.max(...subArray));
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+/**
+ * function largestOfFour(arr) {
+ *   const result = [];
+ *
+ *   for (let i = 0; i < arr.length; i++) {
+ *     let max = arr[i][0];
+ *
+ *     for (let j = 1; j < arr[i].length; j++) {
+ *       if (arr[i][j] > max) {
+ *         max = arr[i][j];
+ *       }
+ *     }
+ *
+ *     result.push(max);
+ *   }
+ *
+ *   return result;
+ * }
+ */
